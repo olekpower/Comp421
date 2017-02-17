4052ee7626a07379f7190c1c24423d52ceb09ad8
Alex Mapley <alexmapley@gmail.com>
2017-02-16 20:41:25 -0500

    Add files via upload

---
+++ DogsDigDogsSetup.sql
@@ -0,0 +1,48 @@
+-- Independent Entity Tables
+DROP TABLE dogProfiles
+DROP TABLE dogStatuses
+DROP TABLE dogPictures
+DROP TABLE dogClubs
+DROP TABLE dogEvents
+DROP TABLE ownerProfiles
+DROP TABLE retailers
+DROP TABLE services
+
+
+CREATE TABLE dogProfiles {
+	did INTEGER NOT NULL,
+	
+	
+}
+
+CREATE TABLE dogProfiles {
+
+}
+
+CREATE TABLE dogPictures {
+
+}
+
+CREATE TABLE dogClubs {
+
+}
+
+CREATE TABLE dogEvents {
+
+}
+
+CREATE TABLE ownerProfiles {
+
+}
+
+CREATE TABLE retailers {
+
+}
+
+CREATE TABLE services {
+
+}
+
+
+
+-- Relational Entity Tables
