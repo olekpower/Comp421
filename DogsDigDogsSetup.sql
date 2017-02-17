41e5fb6ea702121eb80a33949c1c88cf3987fda9
Alex Mapley <alexmapley@gmail.com>
2017-02-16 23:33:54 -0500

    Add files via upload

--- DogsDigDogsSetup.sql
+++ DogsDigDogsSetup.sql
@@ -1,12 +1,12 @@
 -- Independent Entity Tables
---DROP TABLE dogProfiles
---DROP TABLE dogStatuses
---DROP TABLE dogPictures
---DROP TABLE dogClubs
---DROP TABLE dogEvents
---DROP TABLE ownerProfiles
---DROP TABLE retailers
---DROP TABLE services
+DROP TABLE dogProfiles;
+DROP TABLE dogStatuses;
+DROP TABLE dogPictures;
+DROP TABLE dogClubs;
+DROP TABLE dogEvents;
+DROP TABLE ownerProfiles;
+DROP TABLE retailers;
+DROP TABLE services;
 
 
 CREATE TABLE dogProfiles (
@@ -97,21 +97,21 @@ CREATE TABLE retailers (
 
 -- THIS WHOLE SECTION NEEDS CHECKING
 -- Relational Entity Tables
-DROP TABLE dogFriends 
-DROP TABLE familyMembers 
-DROP TABLE relationships  
-DROP TABLE uploadsPhoto 
-DROP TABLE tagsPhoto  
-DROP TABLE makesStatus 
-DROP TABLE membersOfClubs 
-DROP TABLE clubAdmins 
-DROP TABLE goingToEvent 
-DROP TABLE ownsDogs 
-DROP TABLE eventAdmins 
-DROP TABLE servicesSponsorEvents 
-DROP TABLE ownerReservesServices 
-DROP TABLE retailAdmins 
-DROP TABLE serviceAdmins
+DROP TABLE dogFriends;
+DROP TABLE familyMembers; 
+DROP TABLE relationships;
+DROP TABLE uploadsPhoto;
+DROP TABLE tagsPhoto;  
+DROP TABLE makesStatus; 
+DROP TABLE membersOfClubs; 
+DROP TABLE clubAdmins; 
+DROP TABLE goingToEvent; 
+DROP TABLE ownsDogs; 
+DROP TABLE eventAdmins; 
+DROP TABLE servicesSponsorEvents; 
+DROP TABLE ownerReservesServices; 
+DROP TABLE retailAdmins; 
+DROP TABLE serviceAdmins;
 
 
 
