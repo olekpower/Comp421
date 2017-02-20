411de701f177877585dd2c8d74a5c4991626a014
Alex Mapley <alexmapley@gmail.com>
2017-02-19 22:06:56 -0500

    Add files via upload

---
+++ makedata.sql
@@ -0,0 +1,12 @@
+--Make dog Profiles
+INSERT INTO dogProfiles Values ( 1, 'Spike', 1, 0, to_date('2011-03-11', 'YYYY-MM-DD'));
+INSERT INTO dogProfiles Values ( 2, 'Faye', 0, 0, to_date('2007-09-09', 'YYYY-MM-DD'));
+INSERT INTO dogProfiles Values ( 3, 'Jet', 1, 0, to_date('2009-01-28', 'YYYY-MM-DD'));
+INSERT INTO dogProfiles Values ( 4, 'Samantha', 0, 1, to_date('2014-06-17', 'YYYY-MM-DD'));
+INSERT INTO dogProfiles Values ( 5, 'Taco', 1, 1, to_date('2014-04-15', 'YYYY-MM-DD'));
+INSERT INTO dogProfiles Values ( 6, 'Vanessa', 0, 0, to_date('2011-03-11', 'YYYY-MM-DD'));
+INSERT INTO dogProfiles Values ( 7, 'Oscar', 1, 1, to_date('2012-05-11', 'YYYY-MM-DD'));
+INSERT INTO dogProfiles Values ( 8, 'Cocoa', 0, 0, to_date('2016-08-24', 'YYYY-MM-DD'));
+INSERT INTO dogProfiles Values ( 9, 'Hammy', 1, 1, to_date('2017-01-29', 'YYYY-MM-DD'));
+INSERT INTO dogProfiles Values ( 10, 'JellyBeans', 0, 0, to_date('2007-06-16', 'YYYY-MM-DD'));
+
