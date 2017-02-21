51115803c721519f6dfaa80d8e475d009b1bf9a5
Alex Mapley <alexmapley@gmail.com>
2017-02-21 18:30:39 -0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,1 +1,15 @@
-five queries
+--five queries
+
+-- 1) Lists our dogs in descending order by birthdate
+select name, age(birthdate)
+from dogprofiles
+order by age(birthdate) DESC
+
+
+--2
+
+--3
+
+--4
+
+--5
