9beef3f05f4abbadcaf3f6b90370b9ec9d792cbd
Alex Mapley <alexmapley@gmail.com>
2017-02-21 18:49:55 -0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,15 +1,33 @@
---five queries
+-- Consider each one of these a work in progress.
+-- Though maybe not complex enough yet,
+-- they all actually work.
 
 -- 1) Lists our dogs in descending order by birthdate
 select name, age(birthdate)
 from dogprofiles
 order by age(birthdate) DESC
+;
 
 
---2
+-- 2) Lists all statuses
+select * 
+from dogstatuses
+;
 
---3
+--3) Joins Owner and Dog Profiles on shared ID
+select * 
+from dogprofiles
+inner join ownerprofiles
+on (dogprofiles.did = ownerprofiles.opid)
+;
 
---4
+--4) Lists all Clubs
+select * 
+from dogclubs
+;
 
---5
+--5) Lists all services and retailers
+select * 
+from services
+and retailers
+;
